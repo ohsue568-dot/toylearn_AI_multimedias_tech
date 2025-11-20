da24371b58581f038f1e01ae1afc928f9d765af2
ohsue568-dot <ohsue568@gmail.com>
2025-11-20 18:00:09 +0900

    FF

---
+++ quests/webscrappings_databases.sql
@@ -0,0 +1,16 @@
+CREATE TABLE study_webscripings_database (
+    id varchar(500) PRIMARY KEY,
+    contents varchar(500),
+    link varchar(500),
+    link_html varchar(500),
+    link_href varchar(500)
+    created_at varchar(500) DEFAULT NOW()
+);
+
+SELECT *
+        contents varchat(500),
+        link varchat(500),
+        link_html varchat(500),
+        link_href varchat(500),
+    );
+
